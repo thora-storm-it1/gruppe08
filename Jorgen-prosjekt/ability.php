539225bd50fa63cab92116ad5487559fb35eecbc
sobje051201 <48018315+sobje051201@users.noreply.github.com>
2019-03-08 12:47:01 +0100

    7. mars
    Har laget "skjelettet" for nettsiden og brukt photshop til å skaffe bildene jeg trenger.

---
+++ Jorgen-prosjekt/ability.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="stilark.css">
+</head>
+<body>
+  <div class="sidepappa">
+    <div class="bilde">
+      <img src="bilder/comeback.png">
+    </div>
+    <div class="beskrivelse">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat ac dui nec hendrerit. Nulla ut ipsum mi. Suspendisse potenti. Fusce vel nisi ac magna tristique tincidunt. Mauris facilisis enim metus, id ultricies quam eleifend quis. Sed malesuada volutpat ipsum ac suscipit. Aliquam erat volutpat. Phasellus ac ex sapien. Curabitur accumsan ornare accumsan.
+    </div>
+    <div class="tabell">
+      <table>
+        <tr>
+          <th> Navn </th>
+          <th> Stjerner </th>
+          <th> Større sjanse </th>
+          <th> Mindre sjanse </th>
+        </tr>
+        <tr>
+          <td> Hero armour replica </td>
+          <td> 3 </td>
+          <td> Cuttlegear </td>
+          <td> 7-eleven </td>
+        </tr>
+        <tr>
+          <td> Lorem </td>
+          <td> Ipsum </td>
+          <td> ip </td>
+          <td> test </td>
+        </tr>
+      </table>
+    </div>
+</body>
+</html>
---
+++ Jorgen-prosjekt/index.php
@@ -0,0 +1,43 @@
+
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="stilark.css">
+</head>
+<body>
+  <h1> Lorem Ipsum </h1>
+  <div class="pappa">
+    <div class="overgutt">
+      Lorem Ipsum dolor sit amet
+    </div>
+    <div class="undergutt">
+      <a href="ability.php"><img src="bilder/ink_resistance_up.png"></a>
+      <a href="ability.php"><img src="bilder/ink_saver_main.png"></a>
+      <a href="ability.html"><img src="bilder/ink_saver_sub.png"></a>
+      <a href="ability.php"><img src="bilder/ink_recovery_up.png"></a>
+      <a href="ability.php"><img src="bilder/run_speed_up.png"></a>
+      <a href="ability.php"><img src="bilder/swim_speed_up.png"></a>
+      <a href="ability.php"><img src="bilder/special_saver.png"></a>
+      <a href="ability.php"><img src="bilder/special_power_up.png"></a>
+      <a href="ability.php"><img src="bilder/quick_respawn.png"></a>
+      <a href="ability.php"><img src="bilder/quick_super_jump.png"></a>
+      <a href="ability.php"><img src="bilder/sub_power_up.png"></a>
+      <a href="ability.php"><img src="bilder/special_charge_up.png"></a>
+      <a href="ability.php"><img src="bilder/bomb_defense_up_dx.png"></a>
+      <a href="ability.php"><img src="bilder/main_power_up.png"></a>
+      <a href="ability.php"><img src="bilder/ability_doubler.png"></a>
+      <a href="ability.php"><img src="bilder/last_ditch_effort.png"></a>
+      <a href="ability.php"><img src="bilder/tenacity.png"></a>
+      <a href="ability.php"><img src="bilder/comeback.png"></a>
+      <a href="ability.php"><img src="bilder/ninja_squid.png"></a>
+      <a href="ability.php"><img src="bilder/haunt.png"></a>
+      <a href="ability.php"><img src="bilder/stealth_jump.png"></a>
+      <a href="ability.php"><img src="bilder/thermal_ink.png"></a>
+      <a href="ability.php"><img src="bilder/opening_gambit.png"></a>
+      <a href="ability.php"><img src="bilder/respawn_punisher.png"></a>
+      <a href="ability.php"><img src="bilder/object_shredder.png"></a>
+      <a href="ability.php"><img src="bilder/drop_roller.png"></a>
+    </div>
+  </div>
+</body>
+</html>
