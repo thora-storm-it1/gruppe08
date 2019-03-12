6e1a29de3a08dcd9f32c103f967ce2095985e8fd
sobje051201 <48018315+sobje051201@users.noreply.github.com>
2019-03-12 08:34:56 +0100

    8. mars
    Rydder litt i mappestrukturen

---
+++ egenskap.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="stilark.css">
+</head>
+<body>
+  <div class="sidepappa">
+    <div class="bilde">
+      <img src="bilder/comeback.png">
+    </div>
+    <div class="beskrivelse">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat ac dui nec hendrerit. Nulla ut ipsum mi. Suspendisse potenti. Fusce vel nisi ac magna tristique tincidunt. Mauris facilisis enim metus, id ultricies quam eleifend quis. Sed malesuada volutpat ipsum ac suscipit. Aliquam erat volutpat. Phasellus ac ex sapien. Curabitur accumsan ornare accumsan. </p>
+    </div>
+    <div class="tabell">
+      <table>
+        <tr>
+          <th> Navn </th>
+          <th> Stjerner </th>
+          <th> Større sjanse </th>
+          <th> Mindre sjanse </th>
+        </tr>
+        <?php
+        
+        ?>
+      </table>
+    </div>
+</body>
+</html>
